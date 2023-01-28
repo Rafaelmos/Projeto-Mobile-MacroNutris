c2af74359fb6df0a72743570240099aae95266f0
Rafaelmos <rafael.rjms0@gmail.com>
2023-01-28 17:04:39 -0300

    conectando o flutter ao firebase

--- macro_nutris/lib/main.dart
+++ macro_nutris/lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:macro_nutris/widgets/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tela de Login',
       theme: ThemeData(primaryColor: Colors.blue),
       home: const Login(),
-
     );
-    
   }
 }
---
+++ macro_nutris2/android/app/src/main/kotlin/com/example/macro_nutris2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.macro_nutris2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
