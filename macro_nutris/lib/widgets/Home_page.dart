60dd9f3a05c9f06c5ce36b3fa3e27a4db516184b
Rafaéla Moura <63984163+rafaela-moura@users.noreply.github.com>
2023-02-18 09:25:59 -0300

    Update Home_page.dart

--- macro_nutris/lib/widgets/Home_page.dart
+++ macro_nutris/lib/widgets/Home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:macro_nutris/widgets/Checagem_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,10 +10,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _firebaseAuth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Page')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Home Page',
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: () {
+              sair();
+            },
+            child: Text('Sair'),
+          ),
+        ],
+      ),
     );
+  }
+
+  sair() async {
+    await _firebaseAuth.signOut().then(
+          (user) => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Checagem_page(),
+            ),
+          ),
+        );
   }
 }
